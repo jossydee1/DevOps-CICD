645311cc5fc1a4bba76210ab10d140fdd71b326e
Joe <imac@IMACs-MacBook-Air.local>
2024-07-20 19:12:01 +0100

    zarttalent Meme

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,13 +3,13 @@ import "./Header.css";
 import meme from "../../Resources/meme.png";
 
 function Header() {
-  return (
-    <div className="header">
-      <img src={meme} alt="" />
-      <h1>Meme Generator</h1>
-      <p>...get that meme</p>
-    </div>
-  );
+	return (
+		<div className="header">
+			<img src={meme} alt="" />
+			<h1>Zarttalent Meme Generator</h1>
+			<p>...get that meme</p>
+		</div>
+	);
 }
 
 export default Header;
